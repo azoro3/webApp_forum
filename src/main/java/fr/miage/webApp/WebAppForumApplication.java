052f45df3828a9fad1eba4af8b2396e2a2d6c0af
ARTHUR-PC\Arthur <arthur.rozo@outlook.fr>
2017-12-06 16:37:13 +0100

    intial commit

---
+++ src/main/java/fr/miage/webApp/WebAppForumApplication.java
@@ -0,0 +1,12 @@
+package fr.miage.webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppForumApplication.class, args);
+	}
+}
